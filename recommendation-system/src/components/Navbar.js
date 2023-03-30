2c0b447dce1cae2513b765472b44aa65d3bfb8bc
Dinesh Lakshmanan <dineshlakshman2001@gmail.com>
2023-03-30 22:34:53 +0530

    dd

--- recommendation-system/src/components/Navbar.js
+++ recommendation-system/src/components/Navbar.js
@@ -2,9 +2,10 @@ import React from 'react'
 import '../css/bootstrap.css'
 import '../css/navbar.css'
 
+
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-warning-subtle">
+        <nav className="navbar navbar-expand-lg" style={{backgroundColor:'#FADA9D'}}>
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">🏘️ Home</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,6 +14,15 @@ function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
                         <a className="nav-link active" aria-current="page" href="/feedback">📜 Feedback</a>
+                    </div>
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="/fertiliser">🧪 Fertiliser</a>
+                    </div>
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="/weather">🌦️ Weather</a>
+                    </div>
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="/price">🏷 Price</a>
                     </div>
                 </div>
             </div>
